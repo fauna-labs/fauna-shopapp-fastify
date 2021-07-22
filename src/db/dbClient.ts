8e01aca058db051ffce9784cccdfc9d25a227777
Artyom Kurnikov <tigger9flow@gmail.com>
2021-07-22 17:22:22 +0300

    Add header for Fauna client

--- src/db/dbClient.ts
+++ src/db/dbClient.ts
@@ -8,11 +8,17 @@ if (!SECRET) {
   throw new Error('Missing "FAUNA_SECRET_SERVER_KEY" env variable')
 }
 
+const headers = {
+  'x-fauna-source': 'shopapp-fastify',
+}
+
 export const client = new Fauna.Client({
   secret: SECRET,
+  headers,
 })
 
 export const clientForSecret = (secret: string) =>
   new Fauna.Client({
     secret,
+    headers,
   })
